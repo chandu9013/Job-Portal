4e77a9d295957d8d45c223dc8c80c80f1115888e
Sai Chandra Sekhar Dandu <saichandrasekhardandu@Sais-MacBook-Pro.local>
2016-10-26 15:24:09 +0530

    Enabled HTML5 for Angular

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -3,7 +3,9 @@
  */
 var app = angular.module("myApp", [ 'ngRoute',
 		'angularUtils.directives.dirPagination' ]);
-app.config(function($routeProvider) {
+app.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+	$locationProvider.hashPrefix('!');
 	$routeProvider.when('/', {
 		controller : "LoginController",
 		templateUrl : "views/login.html"
